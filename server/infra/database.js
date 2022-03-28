4836ec2e25c5e1d00255e45da306326b52df9060
Inocencio Codeiro <cordeiroturorias@gmail.com>
2022-03-28 14:41:52 +0100

    alterando as credencias para conectar com o banco no heroku

--- server/infra/database.js
+++ server/infra/database.js
@@ -1,10 +1,10 @@
 const pgp = require('pg-promise')();
 const db = pgp({
-    user: 'postgres',
-    password: 'Informatica1999',
-    host: 'localhost',
+    user: 'zpefjozkqyineo',
+    password: 'dc6cfb6a2b0e1ad15be4cdb92b94329f8c668e597242e53b7ade143614b39a07',
+    host: 'ec2-3-229-161-70.compute-1.amazonaws.com',
     port: 5432,
-    database: 'appbook'
+    database: 'd1pe0ki03h6ig2'
 
 });
 module.exports = db;
